a9615a80f2914e8809da06b3f80070677ceb0aaf
cheng <15908575332@163.com>
2025-08-06 15:04:17 +0800

    增加404页面组件和相关样式

---
+++ src/components/404page/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="notFound">
+        <div>
+            <img src="@/assets/img/404page.svg" alt="404">
+        </div>
+        <div>
+            <span>你访问的页面不存在</span>
+            <span>返回首页</span>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+.notFound {
+    @include flexCenter(column, center);
+    height: 100vh;
+
+    img {
+        width: 50%;
+    }
+
+    div {
+        margin: 1rem 0;
+    }
+}
+</style>
