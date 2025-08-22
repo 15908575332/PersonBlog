bab1aa31acd96ed6dc882608d925d4d7e8faecc1
cheng <15908575332@163.com>
2025-08-22 10:51:08 +0800

    删除打印语句

--- server/routes/messageBoardApi.js
+++ server/routes/messageBoardApi.js
@@ -25,7 +25,6 @@ router.post('/setMessgeContent', async (req, res) => {
             }
         });
     } catch (error) {
-        console.log(error)
         res.status(500).json({ error: error.message })
     }
 
